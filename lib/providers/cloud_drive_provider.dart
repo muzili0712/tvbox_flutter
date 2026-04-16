17eb43954f101dca08aafea8ecf24b7727db22d3
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-16 16:19:28 +0800

    Update cloud_drive_provider.dart

---
+++ lib/providers/cloud_drive_provider.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tvbox_flutter/nodejs/nodejs_service.dart';
+import 'package:tvbox_flutter/models/cloud_drive.dart';
+import 'package:tvbox_flutter/constants/app_constants.dart';
+import 'dart:convert';
+
+class CloudDriveProvider extends ChangeNotifier {
+  List<CloudDrive> _drives = [];
+  bool _isLoading = false;
+
+  List<CloudDrive> get drives => _drives;
+  bool get isLoading => _isLoading;
+
+  CloudDriveProvider() {
+    _loadDrives();
+  }
+
+  Future<void> _loadDrives() async {
+    final prefs = await SharedPreferences.getInstance();
+    final drivesJson = prefs.getStringList(AppConstants.keyCloudDrives) ?? [];
+    
+    _drives = drivesJson
+        .map((json) => CloudDrive.fromJson(jsonDecode(json)))
+        .toList();
+    
+    notifyListeners();
+  }
+
+  Future<void> addDrive(CloudDrive drive) async {
+    await NodeJSService.instance.addCloudDrive(drive.type, drive.config);
+    _drives.add(drive);
+    await _saveDrives();
+    notifyListeners();
+  }
+
+  Future<void> removeDrive(String id) async {
+    _drives.removeWhere((d) => d.id == id);
+    await _saveDrives();
+    notifyListeners();
+  }
+
+  Future<void> _saveDrives() async {
+    final prefs = await SharedPreferences.getInstance();
+    final drivesJson = _drives.map((d) => jsonEncode(d.toJson())).toList();
+    await prefs.setStringList(AppConstants.keyCloudDrives, drivesJson);
+  }
+
+  Future<List<DriveFile>> listFiles(String driveId, String path) async {
+    final filesJson = await NodeJSService.instance.listCloudDriveFiles(driveId, path);
+    return filesJson.map((json) => DriveFile.fromJson(json)).toList();
+  }
+
+  Future<String> getPlayUrl(String driveId, String fileId) async {
+    return await NodeJSService.instance.getCloudDrivePlayUrl(driveId, fileId);
+  }
+}
