130fe9c6f29b9c79b218433040a31183051d88c4
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-17 17:27:43 +0800

    Update NodeJSBridge.swift

--- ios/Runner/NodeJSBridge.swift
+++ ios/Runner/NodeJSBridge.swift
@@ -4,17 +4,15 @@ import GCDWebServer
 class NodeJSBridge: NSObject {
     static let shared = NodeJSBridge()
     
-    private var isRunning = false
-    private var nodeServerPort: Int?
     private let webServer = GCDWebServer()
     private let queue = DispatchQueue(label: "com.tvbox.nodejs")
     private var serverStarted = false
+    private let nodeManager = NodeJSManager.shared()
     
     private override init() {
         super.init()
     }
     
-    // MARK: - HTTP Server
     private func ensureWebServerStarted(completion: @escaping (Bool) -> Void) {
         if serverStarted {
             completion(true)
@@ -22,17 +20,14 @@ class NodeJSBridge: NSObject {
         }
         
         queue.async { [weak self] in
-            guard let self = self else {
-                DispatchQueue.main.async { completion(false) }
-                return
-            }
+            guard let self = self else { return }
             
             self.webServer.removeAllHandlers()
             
             self.webServer.addDefaultHandler(forMethod: "GET", request: GCDWebServerRequest.self) { [weak self] request in
                 if request.path == "/onCatPawOpenPort" {
                     if let portStr = request.query?["port"], let port = Int(portStr) {
-                        self?.nodeServerPort = port
+                        self?.nodeManager.setNodeServerPort(Int32(port))
                         print("🐱 Node.js source server port: \(port)")
                     }
                     return GCDWebServerDataResponse(text: "OK")
@@ -66,53 +61,29 @@ class NodeJSBridge: NSObject {
                 }
             }
             
-            if let exception = exception {
-                print("❌ HTTP server exception: \(exception)")
+            if exception != nil {
+                print("❌ HTTP server exception")
                 DispatchQueue.main.async { completion(false) }
             }
         }
     }
     
-    // MARK: - Node.js Startup
     func startNodeJS(completion: @escaping (Bool) -> Void) {
         ensureWebServerStarted { [weak self] success in
-            guard let self = self else {
-                completion(false)
-                return
-            }
+            guard let self = self else { return }
             if !success {
-                print("❌ Web server failed to start, aborting Node.js launch")
                 completion(false)
                 return
             }
             
-            self.queue.async {
-                if self.isRunning {
-                    DispatchQueue.main.async { completion(true) }
-                    return
-                }
-                
-                guard let scriptPath = Bundle.main.path(forResource: "main", ofType: "js", inDirectory: "nodejs-project/dist") else {
-                    print("❌ Node.js script not found")
-                    DispatchQueue.main.async { completion(false) }
-                    return
-                }
-                
-                let args = ["node", scriptPath]
-                var cArgs = args.map { strdup($0) }
-                let argc = Int32(cArgs.count)
-                
-                DispatchQueue.global(qos: .userInitiated).async {
-                    // 直接调用 C 函数
-                    _ = node_start(argc, &cArgs)
-                    for ptr in cArgs { free(ptr) }
-                    self.isRunning = false
-                    print("Node.js exited")
-                }
-                
-                Thread.sleep(forTimeInterval: 2.0)
-                self.isRunning = true
-                DispatchQueue.main.async { completion(true) }
+            guard let scriptPath = Bundle.main.path(forResource: "main", ofType: "js", inDirectory: "nodejs-project/dist") else {
+                print("❌ Node.js script not found")
+                completion(false)
+                return
+            }
+            
+            self.nodeManager.startNodeJS(withScriptPath: scriptPath) { success in
+                completion(success)
             }
         }
     }
@@ -121,31 +92,17 @@ class NodeJSBridge: NSObject {
         queue.async { [weak self] in
             self?.webServer.stop()
             self?.serverStarted = false
-            self?.isRunning = false
+            self?.nodeManager.stopNodeJS()
         }
     }
     
     func sendMessage(_ message: String, completion: ((Result<Any, Error>) -> Void)? = nil) {
-        queue.async { [weak self] in
-            guard let nodePort = self?.nodeServerPort else {
-                completion?(.failure(NSError(domain: "NodeJS", code: -1,
-                                             userInfo: [NSLocalizedDescriptionKey: "Node service port unknown"])))
-                return
+        nodeManager.sendMessage(message) { result, error in
+            if let error = error {
+                completion?(.failure(error))
+            } else {
+                completion?(.success(result ?? NSNull()))
             }
-            
-            let url = URL(string: "http://127.0.0.1:\(nodePort)/msg")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.httpBody = message.data(using: .utf8)
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            
-            URLSession.shared.dataTask(with: request) { _, _, error in
-                if let error = error {
-                    completion?(.failure(error))
-                } else {
-                    completion?(.success(NSNull()))
-                }
-            }.resume()
         }
     }
     
